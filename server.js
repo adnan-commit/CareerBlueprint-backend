235b43372dba3e84a292f7c18a0e0d2eb774721c
juniorcoder02 <132187577+juniorcoder02@users.noreply.github.com>
2026-04-13 10:49:32 +0530

    added root route in server.js

--- server.js
+++ server.js
@@ -13,3 +13,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
